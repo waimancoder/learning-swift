cfc82d696fe8278038995c5723fcb5fb5dd39e4f
waimancoder <waimancoder@gmail.com>
2023-06-14 20:12:27 +0800

    Initial Commit

---
+++ Cryptomanic/CryptomanicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptomanicApp.swift
+//  Cryptomanic
+//
+//  Created by Fakhruddin Aiman on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptomanicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
